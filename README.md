c9dfee038774b3b0dc4b127c406948b7325e6db6
BeaGuti <beatriz.gutierrez@gmail.com>
2024-10-06 14:43:50 +0200

    refactor mvc book example using POO

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Definition
 A software design pattern that divides program logic into three interconnected elements: model, view, and controller.
 Purpose
 Used for developing user interfaces by separating the internal representations of information (model) from the interface (view) and the controlling software (controller).
+
+Usage: `python mvc/book/controller.py`
--- mvc/book/controller.py
+++ mvc/book/controller.py
@@ -5,7 +5,7 @@
 from flask import redirect, render_template, request, Flask
 from werkzeug.exceptions import BadRequest, NotFound
 
-import models
+from models import Url
 
 # Initialize Flask application
 app = Flask(__name__, template_folder="views")
@@ -26,26 +26,33 @@ def shorten():
         raise BadRequest()
 
     # Model returns object with short_url property
-    url_model = models.Url.shorten(full_url)
-    url_model.short_url
+    url = Url(full_url)
 
     # Pass data to view and call its render method
-    short_url = request.host + "/" + url_model.short_url
-    return render_template("success.html", short_url=short_url)
+    short_url = request.host + "/" + url.short_url 
+    return render_template("success_short.html", url=short_url)
 
 
 @app.route("/<path:path>")
 def redirect_to_full(path=""):
     """Gets short url and redirects user to corresponding full url if found."""
     # Model returns object with full_url property
-    url_model = models.Url.get_by_short_url(path)
+    url_model = Url(path)
 
     # Validate model return
     if not url_model:
         raise NotFound()
 
-    return redirect(url_model.full_url)
+    return redirect(url_model.long_url)
 
+@app.route("/lengthen/")
+def lengthen():
+    short_url = request.args.get("url")
+    if not short_url:
+        raise BadRequest()
+    
+    long_url = Url.get_by_short_url(short_url)
+    return render_template("success_long.html", url=long_url)
 
 if __name__ == "__main__":
     app.run(debug=True)
