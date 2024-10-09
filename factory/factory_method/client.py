4d2bbc84b51e5c1bec26f3fecdb3b2a14864640b
BeaGuti <beatriz.gutierrez@gmail.com>
2024-10-09 18:09:04 +0200

    fix protocol without port

--- factory/factory_method/client.py
+++ factory/factory_method/client.py
@@ -1,6 +1,6 @@
-from http_connector import HTTPConnector
-from ftp_connetor import FTPConnector
+from connector import HTTPConnector, FTPConnector
 from urllib.error import URLError
+from urllib.parse import urljoin
 
 if __name__=="__main__":
     domain = 'ftp.freebsd.org'
@@ -13,10 +13,14 @@ if __name__=="__main__":
         connector = HTTPConnector(is_secure=is_secure)
     else:
         connector = FTPConnector(is_secure=False)
-    
+
     try:
+        # url = urljoin(f"{connector.protocol_factory_method()}://{domain}:{connector.port_factory_method()}", path)
+        url = urljoin(f"{connector.protocol_factory_method()}://{domain}", path)
+        print(f"Connecting to {url}")
+
         content = connector.read(domain, path)
+        print(connector.parse(content))
+
     except URLError as e:
         print(f"Can't access resource with this method: {e}")
-    else:
-        print(connector.parse(content))
--- factory/factory_method/connector.py
+++ factory/factory_method/connector.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
 from urllib.request import urlopen
+from port import FTPPort, HHTPort, HTTPSecurePort
+from bs4 import BeautifulSoup
 
 
 class Connector(ABC):
@@ -23,10 +25,11 @@ class Connector(ABC):
         """
         A generic method for reading web content.
         """
-        url = self.protocol + '://' + host + str(self.port) + path
+        # url = self.protocol + '://' + host + str(self.port) + path
+        url = f"{self.protocol}://{host}{path}"
         print(f"Connecting to {url}")
         return urlopen(url, timeout=2).read()
-    
+
     @abstractmethod
     def protocol_factory_method(self) -> str:
         """
@@ -42,4 +45,55 @@ class Connector(ABC):
         pass
 
 
+class HTTPConnector(Connector):
+    """
+    A concrete connector that creates a HTTP connector and sets in runtime all its attributes
+    """
 
+    def protocol_factory_method(self) -> str:
+        if self.is_secure:
+            return "https"
+        return "http"
+
+    def port_factory_method(self):
+        if self.is_secure:
+            return HTTPSecurePort()
+        return HHTPort()
+
+    def parse(self, content):
+        """
+        Parses web content
+        """
+        soup = BeautifulSoup(content, 'lxml')
+        links = soup.table.findAll("a")
+
+        #return soup.prettify()
+        return "\n".join([link["href"] for link in links])
+
+
+class FTPConnector(Connector):
+    """
+    A concrete connector that creates a FTP connector and sets in runtime all its attributes
+    """
+
+    def protocol_factory_method(self) -> str:
+        return "ftp"
+
+    def port_factory_method(self):
+        return FTPPort()
+
+    def parse(self, content):
+        """
+        Parses ftp content
+        """
+        # decode bytes to string
+        lines = content.decode('utf-8').split("\n")
+        filenames = []
+
+        for line in lines:
+            # ftp format has 8 columns
+            splitted_line = line.split(None, 8)
+            if len(splitted_line) == 9:
+                filenames.append(splitted_line[-1])
+
+        return "\n".join(filenames)
